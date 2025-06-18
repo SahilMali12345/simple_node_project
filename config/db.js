844590eec68752d98124cfc22d1ff7f5b86bda89
Sahil Mali <sahil@gmail.com>
2025-06-18 14:09:57 +0530

    Updated commit

--- config/db.js
+++ config/db.js
@@ -1,19 +1,25 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+//##const mongoose = require('mongoose');
+//const config = require('config');
+//const db = config.get('mongoURI');
+
+//const connectDB = async () => {
+//  try {
+//    await mongoose.connect(db, {
+//      useNewUrlParser: true, 
+//     useUnifiedTopology: true
+//    });
+
+//    console.log('MongoDB Connected...');
+//  } catch (err) {
+//    console.error(err.message);
+//    process.exit(1);
+//  }
+//};
+
+//module.exports = connectDB;
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true, 
-      useUnifiedTopology: true
-    });
-
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
+  console.log('⛔ Skipping DB connection. Running in no-DB mode.');
 };
 
 module.exports = connectDB;
